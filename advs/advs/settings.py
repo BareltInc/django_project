03fbee2ad3a31e5ba8a3563f453dc40e2a0aa37f
Eldar <bareltinc@gmail.com>
2023-08-18 15:46:15 +0500

    ver1.working on server :)

--- advs/advs/settings.py
+++ advs/advs/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_advs',
-    'app_lesson_4'
+    'app_lesson_4',
 ]
 
 MIDDLEWARE = [
--- advs/app_lesson_4/urls.py
+++ advs/app_lesson_4/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from .views import lesson_4
 
 urlpatterns = [
-    path('lesson_4/', lesson_4),
+    path('', lesson_4),
 ]
