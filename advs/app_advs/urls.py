d37c533a94c16ed16160d547aa775fc0ad933e5c
Eldar <bareltinc@gmail.com>
2023-08-19 16:12:48 +0500

    ver2.working on server :)

--- advs/app_advs/urls.py
+++ advs/app_advs/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import index
+from .views import index, top_sellers
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='main_page'),
+    path('top-sellers/', top_sellers, name='top-sellers')
 ]
