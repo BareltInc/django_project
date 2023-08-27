b3159c005b05d455967b1586182daba3bd4d8768
Eldar <bareltinc@gmail.com>
2023-08-27 14:55:21 +0500

    les_11 full_working_nav_panel

---
+++ advs/app_auth/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def register(request):
+    return render(request, 'register.html')
